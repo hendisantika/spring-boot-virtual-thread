3b0334615f283108d33791e420fbda22a6bf55bc
hendisantika <hendisantika@yahoo.co.id>
2023-09-23 14:21:21 +0700

    #5 - Modify Main App ✏️📗:octocat:⬆

--- src/main/java/id/s/hendisantika/springbootvirtualthread/SpringBootVirtualThreadApplication.java
+++ src/main/java/id/s/hendisantika/springbootvirtualthread/SpringBootVirtualThreadApplication.java
@@ -2,12 +2,23 @@ package id.s.hendisantika.springbootvirtualthread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class SpringBootVirtualThreadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootVirtualThreadApplication.class, args);
+    }
+
+    @Bean
+    TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
     }
 
 }
