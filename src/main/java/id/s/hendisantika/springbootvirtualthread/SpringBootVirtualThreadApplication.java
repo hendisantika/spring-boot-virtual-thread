c01082887a8ebf37c6e7bff64175f95795d81d54
hendisantika <hendisantika@yahoo.co.id>
2023-09-24 08:18:34 +0700

    #10 - Enable Virtual Thread ✏️📗🐳⬆

--- src/main/java/id/s/hendisantika/springbootvirtualthread/SpringBootVirtualThreadApplication.java
+++ src/main/java/id/s/hendisantika/springbootvirtualthread/SpringBootVirtualThreadApplication.java
@@ -1,5 +1,7 @@
 package id.s.hendisantika.springbootvirtualthread;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
@@ -10,15 +12,19 @@ import java.util.concurrent.Executors;
 @SpringBootApplication
 public class SpringBootVirtualThreadApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootVirtualThreadApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootVirtualThreadApplication.class, args);
     }
 
     @Bean
-    TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
         return protocolHandler -> {
+            LOGGER.info("Configuring " + protocolHandler + " to use VirtualThreadPerTaskExecutor");
             protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
         };
     }
+
 
 }
