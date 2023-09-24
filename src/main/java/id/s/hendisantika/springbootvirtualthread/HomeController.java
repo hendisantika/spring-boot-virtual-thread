66a54b59b89630d54e67e74754a55d2fad3843df
hendisantika <hendisantika@yahoo.co.id>
2023-09-24 08:19:28 +0700

    #11 - Add Virtual Thread API ✏️📗🐳⬆

--- src/main/java/id/s/hendisantika/springbootvirtualthread/HomeController.java
+++ src/main/java/id/s/hendisantika/springbootvirtualthread/HomeController.java
@@ -1,9 +1,17 @@
 package id.s.hendisantika.springbootvirtualthread;
 
+import id.s.hendisantika.springbootvirtualthread.model.Product;
+import id.s.hendisantika.springbootvirtualthread.repository.ProductRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,14 +24,34 @@ import org.springframework.web.bind.annotation.RestController;
  * To change this template use File | Settings | File Templates.
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class HomeController {
     @Autowired
     private HomeService homeService;
 
-    @GetMapping("/home")
-    public String getResponse() {
+    @Autowired
+    private ProductRepository productRepository;
 
+    @GetMapping("/")
+    public String getResponse() {
         return homeService.getResponse();
+    }
+
+    @GetMapping("/thread")
+    public List<Product> checkThread() throws InterruptedException {
+        Thread.sleep(1000);
+        return productRepository.findAll();
+    }
+
+    @PostMapping("/save")
+    public String saveProduct() throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            Product product = new Product();
+            product.setProductName(RandomStringUtils.randomAlphanumeric(5));
+            product.setPrice(RandomUtils.nextLong(10, 1000));
+            product.setPrice(1L);
+            productRepository.save(product);
+        }
+        return "hendi " + LocalDateTime.now();
     }
 }
